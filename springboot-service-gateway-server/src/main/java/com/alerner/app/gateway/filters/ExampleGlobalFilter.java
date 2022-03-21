d4737d4805aaaf11d49cd976930dc439bda3bbb8
Andres Lerner <andreslerner99@gmail.com>
2022-03-21 09:14:12 -0300

    Added spring cloud gateway and pre/post filter

---
+++ springboot-service-gateway-server/src/main/java/com/alerner/app/gateway/filters/ExampleGlobalFilter.java
@@ -0,0 +1,46 @@
+package com.alerner.app.gateway.filters;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class ExampleGlobalFilter implements GlobalFilter, Ordered
+{
+
+	private final Logger logger = LoggerFactory.getLogger(ExampleGlobalFilter.class);
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) 
+	{
+		logger.info("Execute Filter pre");
+		exchange.getRequest().mutate().headers(h-> h.add("token","123456"));	
+		return chain.filter(exchange).then(Mono.fromRunnable(()->{
+			logger.info("Execute filter post");
+			
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(value -> {
+				exchange.getResponse().getHeaders().add("token",value);
+			});
+			
+			
+			exchange.getResponse().getCookies().add("colour", ResponseCookie.from("colour", "red").build());
+			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+		}));
+	}
+
+	@Override
+	public int getOrder() {
+		return -1;
+	}
+
+}
