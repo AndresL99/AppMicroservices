b552419c8ecd743dba637615af05557a74d1dea0
Andres <andreslerner99@gmail.com>
2022-04-08 09:31:19 -0300

    Implement oauth

---
+++ springboot-service-oauth/src/main/java/com/alerner/app/users/oauth/SpringbootServiceOauthApplication.java
@@ -0,0 +1,15 @@
+package com.alerner.app.users.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringbootServiceOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceOauthApplication.class, args);
+	}
+
+}
